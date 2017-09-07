d626d3d127dc16ce92fce8edd97ae2a74e8926e4
Ricky Amparo <rickyamparo@gmail.com>
2017-09-07 17:20:57 -0600

    create login and register routes, create links in welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
+
+  get '/login', to: 'account#login'
+  get '/register', to: 'account#register'
 end
--- spec/features/visitors/visitor_can_see_root_page_spec.rb
+++ spec/features/visitors/visitor_can_see_root_page_spec.rb
@@ -4,12 +4,14 @@ feature "Visitor can visit the root and see a welcome page" do
   scenario "a visitor visits the home page and sees the welcome page" do
     visit "/"
 
-    expect(page).to have_link(new_user_path)
-    expect(page).to have_link(login_path)
+    expect(page).to have_content("Welcome to the Pokenalysis App!")
 
-    expect(page).to_not have_link(battle_path)
-    expect(page).to_not have_link(pokemon_db_path)
-    expect(page).to_not have_link(analysis_path)
+    expect(page).to have_link("Log in")
+    expect(page).to have_link("Register Account")
+
+    expect(page).to_not have_link("The Battle Page")
+    expect(page).to_not have_link("Pokemon Database")
+    expect(page).to_not have_link("Battle Analysis")
   end
 
   scenario "a visitor can click on register account to fill out an account form" do
