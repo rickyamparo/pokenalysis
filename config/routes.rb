36fe99de42532a89dce01ced4f5100381b71f1bc
Ricky Amparo <rickyamparo@gmail.com>
2017-09-09 14:05:09 -0600

    add battle route to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/battle', to: 'pokemons#battle'
 
   resources :users, only: [:new, :create]
 end
