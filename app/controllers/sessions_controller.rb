1e2f88be7cadafe2e21418b26630cddf3834a349
Ricky Amparo <rickyamparo@gmail.com>
2017-09-09 13:53:21 -0600

    add helper method to application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.clear
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
+  end
+
 end
