640ad4cad9605a91e78b769d25f213502481e3fe
Ricky Amparo <rickyamparo@gmail.com>
2017-09-09 14:01:36 -0600

    change login flash method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      flash[:notice] = "Successful Login"
+      flash[:notice] = "Hello #{@user.name}, you are now signed in"
       redirect_to root_path
     else
       flash[:failure] = "That login was unsuccessful"
