7f66299c99051acfe02ef5f00d24d85b104c4b67
Ricky Amparo <rickyamparo@gmail.com>
2017-09-10 18:42:44 -0600

    add login link for battle analysis to welcome page

--- spec/features/admins/admin_can_visit_root_and_see_links_spec.rb
+++ spec/features/admins/admin_can_visit_root_and_see_links_spec.rb
@@ -6,4 +6,19 @@ feature "Admin can visit the root page and see a welcome page" do
 
     expect(admin.role).to eq("admin")
   end
+
+  scenario "an admin visits the root page and sees all links" do
+    admin = User.create(name: "test", email: "test@test.com", password: "testing", role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/'
+
+    expect(page).to have_link("The Battle Page")
+    expect(page).to have_link("Pokemon Database")
+    expect(page).to have_link("Logout")
+    expect(page).to have_link("Battle Analysis")
+    
+    expect(page).to_not have_link("Register Account")
+    expect(page).to_not have_link("Log in")
+  end
 end
