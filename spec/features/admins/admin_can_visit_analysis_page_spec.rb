1dbe1d40d830aafcd8ae378b1ec788b4901cbf17
Ricky Amparo <rickyamparo@gmail.com>
2017-09-10 19:43:14 -0600

    add setup piece to admin can view user's battles

--- spec/features/admins/admin_can_visit_analysis_page_spec.rb
+++ spec/features/admins/admin_can_visit_analysis_page_spec.rb
@@ -14,11 +14,17 @@ feature "Admin can visit Battle Analysis Page" do
 
   scenario "an admin can select users and view their battles" do
     admin = User.create(name: "test", email: "test@test.com", password: "testing", role: 1)
+
     user_1 = User.create(name: "ricky", email: "ricky@ricky.com", password: "rickying")
     user_2 = User.create(name: "stu", email: "stu@stu.com", password: "stuing")
+
     pokemon_1 = Pokemon.create(name: "Pikachu", health: 100, power: 100, speed: 200)
     pokemon_2 = Pokemon.create(name: "Clefairy", health: 200, power: 50, speed: 100)
-    
+
+    battle = Battle.create(winner: pokemon_1, pokemon_1: pokemon_1.name, pokemon_2: pokemon_2.name)
+
+    PokemonBattle.create(pokemon: pokemon_1, battle: battle)
+    PokemonBattle.create(pokemon: pokemon_2, battle: battle)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
