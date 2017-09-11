3aeb2d586fd8fbf743c02cde7b42f465d25d43da
Ricky Amparo <rickyamparo@gmail.com>
2017-09-10 20:00:45 -0600

    add user and battle associations

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,4 +2,5 @@ class Battle < ApplicationRecord
   validates :winner, :pokemon_1, :pokemon_2, presence: true
   has_many :pokemon_battles
   has_many :pokemon, through: :pokemon_battles
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   validates :name, :password, presence: true
   validates :email, presence: true, uniqueness: true
 
+  has_many :battles
+
   enum role: ["default", "admin"]
 end
