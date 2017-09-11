1e18436fd394be1f429ac5af89bb712d1e7f1ecc
Ricky Amparo <rickyamparo@gmail.com>
2017-09-10 19:08:43 -0600

    add drop down to the analysis index page to slect all users

--- spec/features/admins/admin_can_visit_analysis_page_spec.rb
+++ spec/features/admins/admin_can_visit_analysis_page_spec.rb
@@ -11,4 +11,13 @@ feature "Admin can visit Battle Analysis Page" do
 
     expect(current_path).to eq(battle_analysis_path)
   end
+
+  scenario "an admin can select users and view their battles" do
+    admin = User.create(name: "test", email: "test@test.com", password: "testing", role: 1)
+    user_1 = User.create(name: "ricky", email: "ricky@ricky.com", password: "rickying")
+    user_2 = User.create(name: "stu", email: "stu@stu.com", password: "stuing")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    
+  end
 end
